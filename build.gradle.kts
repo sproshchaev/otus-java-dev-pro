1e58fc50f0f72fb52a2f85cf41131000284ca10b
sproshchaev <sproshchaev@gmail.com>
2026-07-28 21:42:23 +0500

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "com.prosoft"
@@ -10,7 +11,12 @@ repositories {
 }
 
 dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
 
+application {
+    mainClass.set("com.prosoft.Main")
 }
 
 tasks.test {
